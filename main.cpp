9cd62482f2cb54a57e06d7aa2d3149698c64bd39
nechyo <iseullee7@gmail.com>
2024-07-24 01:54:44 -0400

    fix: bin_read function

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string>
 #include <netinet/in.h>
 
 
 uint32_t bin_read(char* path) {
     FILE *fp;
-    fp = fopen(path, "r");
-    if (fp == NULL) return (uint32_t)1;
+    fp = fopen(path, "rb");
+    
+    if (fp == NULL) {
+        printf("error : file mismatch\n");
+        return (uint32_t)1;
+    }
 
     fseek(fp, 0, SEEK_END);
     size_t size = ftell(fp);
     fseek(fp, 0, SEEK_SET);
-    if (size != 4) return (uint32_t)1;
+
+    if (size != 4) {
+        printf("error : file size mismatch\n");
+        fclose(fp);
+        return (uint32_t)1;
+    }
 
     uint32_t output[4];
-    fread(output, sizeof(output), 1, fp);
+    fread(&output, sizeof(output), 1, fp);
     fclose(fp);
     return ntohl(*output);
 }
@@ -27,10 +37,8 @@ int main(int argc, char* argv[]) {
 
     uint32_t bin1 = bin_read(argv[1]);
     uint32_t bin2 = bin_read(argv[2]);
-    if (bin1 == 1 || bin2 == 1) {
-        printf("error : file size mismatch\n");
-        return 1;
-    }
+    if (bin1 == 1 || bin2 == 1) return 1;
+
     uint32_t res = bin1 + bin2;
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", bin1, bin1, bin2, bin2, res, res);
 
